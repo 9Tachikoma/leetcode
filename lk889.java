5aa51ad0f2aa7463120c46c2337788c82d178885
Tachikoma <mr.sesame@outlook.com>
2022-03-25 21:00:49 +0800

    start from tree

---
+++ lk889.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return build(preorder, 0, preorder.length - 1,
+                postorder, 0, postorder.length - 1);
+    }
+
+    TreeNode build(int[] preorder, int preStart, int preEnd,
+                   int[] postorder, int postStart, int postEnd) {
+        if (preStart > preEnd) {
+            return null;
+        }
+        if (preStart == preEnd) {
+            return new TreeNode(preorder[preStart]);
+        }
+
+        int rootVal = preorder[preStart];
+        int leftRootVal = preorder[preStart + 1];
+        int index = 0;
+        for (int i = postStart; i < postEnd; i++) {
+            if (postorder[i] == leftRootVal) {
+                index = i;
+                break;
+            }
+        }
+
+        int leftSize = index - postStart + 1;
+
+        TreeNode root = new TreeNode(rootVal);
+        root.left = build(preorder, preStart + 1, preStart + leftSize,
+                postorder, postStart, index);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                postorder, index + 1, postEnd - 1);
+
+        return root;
+    }
+}
