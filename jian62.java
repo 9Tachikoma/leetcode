6007b54c5285f5a34961ac20a0142bc4a415ee6f
Tachikoma <mr.sesame@outlook.com>
2022-03-31 23:00:48 +0800

    start from tree

---
+++ jian62.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lastRemaining(int n, int m) {
+        return f(n, m);
+    }
+
+    public int f(int n, int m) {
+        if (n == 1) {
+            return 0;
+        }
+        int x = f(n - 1, m);
+        return (m % n + x) % n;
+    }
+}
