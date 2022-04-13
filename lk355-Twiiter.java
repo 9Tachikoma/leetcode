085d96734d9e01633232f10ea4e40b8df96cd2f7
Tachikoma <mr.sesame@outlook.com>
2022-04-14 00:12:52 +0800

    simple Twitter

--- lk355-Twiiter.java
+++ lk355-Twiiter.java
@@ -19,66 +19,93 @@ class Twitter {
         // 用户发表的推文链表头结点
         public Tweet head;
 
-        public User(int id) {
+        public User(int userId) {
             followed = new HashSet<>();
-            this.id = id;
+            this.id = userId;
             this.head = null;
             //关注一下自己
-            followed(id);
+            follow(id);
         }
 
-        public void follow(int UserId) {
-            followed.add(UserId);
+        public void follow(int userId) {
+            followed.add(userId);
         }
 
-        public void unfollow(int UserId) {
-            if (UserId != this.id)
-                followed.remove(UserId);
+        public void unfollow(int userId) {
+            //不可以取关自己
+            if (userId != this.id)
+                followed.remove(userId);
         }
 
         public void post(int tweetId) {
             Tweet twt = new Tweet(tweetId, timestamp);
             timestamp++;
             // 将新建的推文插入链表头
+            // 越靠前的推文 time 值越大
             twt.next = head;
             head = twt;
         }
     }
 
     //用一个映射将userId与User对应起来
-    private HashMap<Integer,User> userMap = new HashMap<>();
+    private HashMap<Integer, User> userMap = new HashMap<>();
 
     public void postTweet(int userId, int tweetId) {
-        if(!userMap.containsKey(userId))
-            userMap.put(userId,new User(userId));
+        // 若 userId 不存在，则新建
+        if (!userMap.containsKey(userId))
+            userMap.put(userId, new User(userId));
         User u = userMap.get(userId);
         u.post(tweetId);
     }
 
     public void follow(int followerId, int followeeId) {
-        if(!userMap.containsKey(followerId)){
-            User u = new User(followerId)
-            userMap.put(followerId,u);
+        if (!userMap.containsKey(followerId)) {
+            User u = new User(followerId);
+            userMap.put(followerId, u);
         }
-        if(!userMap.containsKey(followeeId)){
-            User u = new User(followeeId)
-            userMap.put(followeeId,u);
+        if (!userMap.containsKey(followeeId)) {
+            User u = new User(followeeId);
+            userMap.put(followeeId, u);
         }
-        userMap.get(followeeId).follow(followeeId);
+        userMap.get(followerId).follow(followeeId);
     }
 
     public void unfollow(int followerId, int followeeId) {
-        if(userMap.containKey(followerId)){
-            User flwer = userMap.get(followeeId);
-            flwer.unfollow(followerId);
+        if (userMap.containsKey(followerId)) {
+            User flwer = userMap.get(followerId);
+            flwer.unfollow(followeeId);
         }
     }
 
 
     public List<Integer> getNewsFeed(int userId) {
+        List<Integer> res = new ArrayList<>();
+        if (!userMap.containsKey(userId)) return res;
+        // 关注列表的用户 Id
+        Set<Integer> users = userMap.get(userId).followed;
+        // 自动通过 time 属性从大到小排序，容量为 users 的大小
+        PriorityQueue<Tweet> pq =
+                new PriorityQueue<>(users.size(), (a, b) -> (b.time - a.time));
 
+        // 先将所有链表头节点插入优先级队列
+        for (int id : users) {
+            Tweet twt = userMap.get(id).head;
+            if (twt == null) continue;
+            pq.add(twt);
+        }
+
+        while (!pq.isEmpty()) {
+            // 最多返回 10 条就够了
+            if (res.size() == 10) break;
+            // 弹出 time 值最大的（最近发表的）
+            Tweet twt = pq.poll();
+            res.add(twt.id);
+            // 将下一篇 Tweet 插入进行排序
+            if (twt.next != null)
+                pq.add(twt.next);
+        }
+        return res;
     }
-
 }
 
 /**
