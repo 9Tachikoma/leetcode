cc72920cb03e914f4c27aeb8c720a00415acf627
Tachikoma <mr.sesame@outlook.com>
2022-05-07 10:22:23 +0800

    不是很明显的差分数组

--- lk167.java
+++ lk167.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0, right = numbers.length - 1;
         while (left < right) {
-            sum = numbers[left] + numbers[right];
+            int sum = numbers[left] + numbers[right];
             if (sum == target) {
                 return new int[]{left + 1, right + 1};
             } else if (sum < target) {
